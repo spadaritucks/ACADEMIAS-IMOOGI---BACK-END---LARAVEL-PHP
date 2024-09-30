9e6fdea2a3a9bf0cdac0f7881d8e8af2f4fcf104
spadaritucks <thiago.spadari02@gmail.com>
2024-09-30 13:59:47 -0300

    commit

--- app/Http/Controllers/PlanosController.php
+++ app/Http/Controllers/PlanosController.php
@@ -14,12 +14,12 @@ class PlanosController extends Controller
 {
     public function index (): JsonResponse {
         try {
-            $planos = Planos::all(); // Recupera todos os usuários cadastrados
+            $planos = Planos::all();
 
             return response()->json([
                 'status' => true,
                 'planos' => $planos,
-            ], 200);
+            ], 200)->header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0');
 
         } catch (Exception $e) {
             return response()->json([
@@ -30,14 +30,14 @@ class PlanosController extends Controller
 
     }
 
-    public function show ($id){
-           try {
-            $plano = Planos::findOrFail($id); // Busca o usuário pelo ID
+    public function show ($id): JsonResponse {
+        try {
+            $plano = Planos::findOrFail($id);
 
             return response()->json([
                 'status' => true,
                 'plano' => $plano,
-            ], 200);
+            ], 200)->header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0');
         } catch (Exception $e) {
             return response()->json([
                 'status' => false,
@@ -46,9 +46,8 @@ class PlanosController extends Controller
         }
     }
 
-    public function store (PlanosRequest $request){
-        
-        try{
+    public function store (PlanosRequest $request): JsonResponse {
+        try {
             DB::beginTransaction();
 
             $plano = Planos::create([
@@ -61,8 +60,6 @@ class PlanosController extends Controller
                 'status' => $request->status,
                 'number_checkins' => $request->number_checkins
             ]);
-    
-
 
             DB::commit();
 
@@ -70,21 +67,19 @@ class PlanosController extends Controller
                 'status' => true,
                 'plano' => $plano,
                 'message' => 'Plano cadastrado com Sucesso!'
+            ], 201)->header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0');
 
-            ], 201);
-
-        }catch(Exception $e){
-            
+        } catch (Exception $e) {
+            DB::rollBack(); // Reverte a transação em caso de erro
             return response()->json([
                 'status' => false,
-                'message' => 'Plano não cadastrado!' . $e->getMessage(),
-
+                'message' => 'Plano não cadastrado! ' . $e->getMessage(),
             ], 400);
         }
     }
 
-    public function update (PlanosUpdateRequest $request, $id){
-        try{
+    public function update (PlanosUpdateRequest $request, $id): JsonResponse {
+        try {
             DB::beginTransaction();
 
             $plano = Planos::findOrFail($id);
@@ -99,8 +94,6 @@ class PlanosController extends Controller
                 'status' => $request->status,
                 'number_checkins' => $request->number_checkins
             ]);
-    
-
 
             DB::commit();
 
@@ -108,22 +101,19 @@ class PlanosController extends Controller
                 'status' => true,
                 'plano' => $plano,
                 'message' => 'Plano Atualizado com Sucesso!'
+            ], 201)->header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0');
 
-            ], 201);
-
-        }catch(Exception $e){
-            
+        } catch (Exception $e) {
+            DB::rollBack(); // Reverte a transação em caso de erro
             return response()->json([
                 'status' => false,
-                'message' => 'Plano não atualizado!' . $e->getMessage(),
-
+                'message' => 'Plano não atualizado! ' . $e->getMessage(),
             ], 400);
         }
     }
 
-    public function destroy ($id){
+    public function destroy ($id): JsonResponse {
         try {
-
             $plano = Planos::findOrFail($id);
             $plano->delete();
 
@@ -131,13 +121,12 @@ class PlanosController extends Controller
                 'status' => true,
                 'plano' => $plano,
                 'message' => 'Plano Apagado com Sucesso!'
+            ], 200)->header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0');
 
-            ], 200);
         } catch (Exception $e) {
             return response()->json([
                 'status' => false,
-                'message' => 'Plano não Apagado!' . $e->getMessage()
-
+                'message' => 'Plano não Apagado! ' . $e->getMessage(),
             ], 400);
         }
     }
