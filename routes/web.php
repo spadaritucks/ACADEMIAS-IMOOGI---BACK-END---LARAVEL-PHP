c49475ad88fcad0e021078c841e71952b2eb9709
spadaritucks <thiago.spadari02@gmail.com>
2025-05-30 11:42:51 -0300

    Corrige o caminho da rota de upload para remover a pasta 'uploads', garantindo acesso direto aos arquivos no armazenamento público.

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ Route::get('/', function () {
 
 
 Route::get('/storage/upload/{filename}', function ($filename) {
-    $path = storage_path('app/public/uploads/' . $filename);
+    $path = storage_path('app/public/' . $filename);
 
     if (!File::exists($path)) {
         abort(404);
